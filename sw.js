5b2140572f7e363da51791eaaaf8a89005554a63
JonatSrG <JonatSrG>
2022-10-03 21:28:30 -0500

    SW: Fecth Event

--- sw.js
+++ sw.js
@@ -1,2 +1,12 @@
 //Aqui el service worked
-console.log('SW: Hola Mundo');
+self.addEventListener('fetch', event => {
+
+    if (event.request.url.incluides('styles.css')) {
+        event.respondWith(null);
+    } else {
+        //console.log( event );
+        event.respondWith(fetch(event.request));
+
+    }
+
+});
