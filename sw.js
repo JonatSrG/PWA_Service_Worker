73979d1e5c4fb747e77a349e6c8c95fc9be82aa0
JonatSrG <JonatSrG>
2022-10-03 21:47:19 -0500

    Modificando peticion fetch

--- sw.js
+++ sw.js
@@ -1,16 +1,31 @@
 //Aqui el service worked
 self.addEventListener('fetch', event => {
 
-    //console.log(event.request.url.includes('.jpg'));
-    if ( event.request.url.includes('.jpg') ){
-        console.log(event.request.url);
+    //    console.log(event.request.url.includes('.jpg'));
+    //    if ( event.request.url.includes('.jpg') ){
+    //    console.log(event.request.url);
 
-        //let fotoReq = fetch('img/main.jpg');
-        //let fotoReq = fetch( event.request.url);
+    //    let fotoReq = fetch('img/main.jpg');
+    //    let fotoReq = fetch( event.request.url);
 
-        let fotoReq = fetch( event.request );
+    //    let fotoReq = fetch( event.request );
 
-        event.respondWith( fotoReq );
+    //    event.respondWith( fotoReq );
+
+    if (event.request.url.includes('styles.css')) {
+        
+        let respuesta = new Response(`
+            body {
+                background-color: red !important;
+                color: pink;
+            }
+        `, {
+            headers: {
+                'Content-Type': 'text/css'
+            }
+        });
+
+        event.respondWith( respuesta );
     }
 
 });
